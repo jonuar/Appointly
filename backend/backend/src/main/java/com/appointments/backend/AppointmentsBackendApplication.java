971b7056d3751f6fa67cc6ef58f31f3d2e77beb5
J <joshuanunezarcila@gmail.com>
2025-06-30 03:15:20 +0200

    Project init

---
+++ backend/backend/src/main/java/com/appointments/backend/AppointmentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.appointments.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/appointments/backend/AppointmentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointments.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
